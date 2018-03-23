6083e420150d39a2b9fdb1222c5aa9432b351fa4
Damian404 <sgt,curren@gmail.com>
2018-03-23 23:46:48 +0000

    setup route and view

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', routes);
+
 module.exports = app;
