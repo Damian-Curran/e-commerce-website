b15304cb183e1f5ca7e8c46fefe45cf9de50b617
Damian404 <sgt,curren@gmail.com>
2018-03-23 20:04:29 +0000

    installing expected dependencies

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+module.exports = app;
