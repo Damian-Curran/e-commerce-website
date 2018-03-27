49a345b8828976862ea28f8a877b895d44a85d93
Damian404 <sgt,curren@gmail.com>
2018-03-27 16:33:24 +0100

    attempted file upload through angular

--- app.js
+++ app.js
@@ -9,10 +9,30 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var multer = require('multer');
 
 var routes = require('./routes/index');
 var products = require('./routes/products');
 var users = require('./routes/users');
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpeg|jpg)$/)) {
+            var err = new Error();
+            err.code = 'filetype';
+            return cb(err);
+        } else {
+            cb(null, file.originalname);
+        }
+    }
+});
+var upload = multer({
+    storage: storage,
+    limits: { fileSize: 10000000 }
+}).single('myfile');
 
 // load mongoose package
 var mongoose = require('mongoose');
@@ -26,6 +46,27 @@ mongoose.connect('mongodb://localhost/e-commerce')
 .catch((err) => console.error(err));
 
 var app = express();
+
+app.post('/upload', function(req, res) {
+    console.log("hello i am in app.js");
+    upload(req, res, function(err) {
+        if (err) {
+            if (err.code === 'LIMIT_FILE_SIZE') {
+                res.json({ success: false, message: 'File size is too large. Max limit is 10MB' });
+            } else if (err.code === 'filetype') {
+                res.json({ success: false, message: 'Filetype is invalid. Must be .png' });
+            } else {
+                res.json({ success: false, message: 'Unable to upload file' });
+            }
+        } else {
+            if (!req.file) {
+                res.json({ success: false, message: 'No file was selected' });
+            } else {
+                res.json({ success: true, message: 'File uploaded!' });
+            }
+        }
+    });
+});
 
 // Express Session
 app.use(session({
