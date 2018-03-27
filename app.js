b078dbc595ba9c434f6bbf23d3d606694cb73e68
Damian404 <sgt,curren@gmail.com>
2018-03-27 21:20:51 +0100

    display image of product functionality

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ app.set('view engine', 'ejs');
 
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/views', express.static(__dirname + '/views'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 // BodyParser Middleware
 app.use(bodyParser.json());
