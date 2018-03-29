2a37b9f66b48d9564b02aff4ba190657a53a006c
Damian404 <sgt,curren@gmail.com>
2018-03-29 18:26:48 +0100

    create token, error handling

--- routes/cards.js
+++ routes/cards.js
@@ -5,6 +5,25 @@ var stripe = require('stripe')("sk_test_VZK01VfHuuOdrwNG6E21Q7N4");
 router.post('/:id', function(req, res, next) {
     console.log("req.body " + req.body);
 
+    stripe.tokens.create({
+        
+       card: { "number": req.body.number,
+        "exp_month": req.body.month,
+        "exp_year": req.body.year,
+        "cvc": req.body.cvc,
+        }
+    }, function(err, token) {
+        if(err != null)
+        {
+            console.log(err.message);
+            res.json(err.msg);
+        }
+        else{
+            console.log(token);
+            res.json(token);
+        }
+    });
+
 });
 
 module.exports = router;
