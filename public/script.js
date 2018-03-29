72e4a838431c76d195afb36d7ec78991f69a69be
Damian404 <sgt,curren@gmail.com>
2018-03-29 18:19:57 +0100

    add stripe dependency

--- public/script.js
+++ public/script.js
@@ -179,11 +179,12 @@ angular.module('app', ['ngRoute', 'ngResource'])
 }])
 .controller('cardController', ['$scope', '$routeParams', '$location', 'indexService', 'Authentication', '$resource', 'Cards', function ($scope, $routeParams, $location, indexService, Authentication, $resource, Cards) {
   var user = indexService.user;
+  var User = $resource('/cards/:id', {id: user});
 
   $scope.create = function(){
     var card = new Cards({number: $scope.card.number, cvc: $scope.card.cvc, month: $scope.card.exp_month, year: $scope.card.exp_year});
-
-    console.log(card);
+    User.save(card, function(){
+    });
   }
 }])
 //---------------
