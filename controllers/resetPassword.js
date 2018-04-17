1b016b49cfc929ca3fa2fa3278347f4aaf0f1604
Damian404 <sgt,curren@gmail.com>
2018-04-17 18:23:42 +0100

    git didn't include controller or view changes

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -3,9 +3,16 @@ angular.module('app')
     var user = indexService.user;
   
     var User = $resource('/users/reset/:username', {username: '@username'});
+    var Update = $resource('/users/newPassword/:password/:token', {password: '@pass', token: $routeParams.token});
 
     $scope.submit = function(){
       var username = $scope.username;
       User.save({username:username});
     }
+
+    $scope.newPassword = function(){
+        var pass = $scope.password;
+        Update.save({pass: pass});
+        $location.url("/login");
+      }
   }])
